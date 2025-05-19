de24440183f8177291bdf87505d5e9ce8ef1d660
Yoojin <81798918+y-00jin@users.noreply.github.com>
2025-05-20 01:00:30 +0900

    FEAT: 포인트 조회, 내역 조회, 충전, 사용 비즈니스 로직 구현
    - 포인트 조회, 포인트 충전 및 사용 내역 조회, 포인트 충전, 포인트 사용 비즈니스 로직 구현
    - 포인트 충전 시 최대 잔고를 2,000,000원으로 제한
    - UserPointTable, PointHistoryTable에 대해 별도의 Repository 분리를 고려했으나, 테스트 코드 작성에 집중하기 위해 직접 주입하여 사용

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -1,7 +1,5 @@
 package io.hhplus.tdd.point;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,47 +8,44 @@ import java.util.List;
 @RequestMapping("/point")
 public class PointController {
 
-    private static final Logger log = LoggerFactory.getLogger(PointController.class);
+
+//    private static final Logger log = LoggerFactory.getLogger(PointController.class);
+
+    private final PointService pointService;
+
+    public PointController(PointService pointService) {
+        this.pointService = pointService;
+    }
 
     /**
      * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
      */
     @GetMapping("{id}")
-    public UserPoint point(
-            @PathVariable long id
-    ) {
-        return new UserPoint(0, 0, 0);
+    public UserPoint point(@PathVariable long id) {
+        return pointService.point(id);
     }
 
     /**
      * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
      */
     @GetMapping("{id}/histories")
-    public List<PointHistory> history(
-            @PathVariable long id
-    ) {
-        return List.of();
+    public List<PointHistory> history(@PathVariable long id) {
+        return pointService.history(id);
     }
 
     /**
      * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
      */
     @PatchMapping("{id}/charge")
-    public UserPoint charge(
-            @PathVariable long id,
-            @RequestBody long amount
-    ) {
-        return new UserPoint(0, 0, 0);
+    public UserPoint charge(@PathVariable long id, @RequestBody long amount) {
+        return pointService.charge(id, amount);
     }
 
     /**
      * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
      */
     @PatchMapping("{id}/use")
-    public UserPoint use(
-            @PathVariable long id,
-            @RequestBody long amount
-    ) {
-        return new UserPoint(0, 0, 0);
+    public UserPoint use(@PathVariable long id, @RequestBody long amount) {
+        return pointService.use(id, amount);
     }
 }
---
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -0,0 +1,100 @@
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PointService {
+
+    private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
+
+    private static final long MAX_POINT = 2_000_000L;   // 최대 잔고
+
+
+    /**
+     * # Method설명 : UserPointTable, PointHistoryTable 주입
+     * - Repository 생성을 고민했지만 이미 두 컴포넌트가 Repository 역할을 하고 있어 바로 주입
+     * # MethodName : PointService
+     **/
+    public PointService(UserPointTable userPointTable, PointHistoryTable pointHistoryTable) {
+        this.userPointTable = userPointTable;
+        this.pointHistoryTable = pointHistoryTable;
+    }
+
+
+    /**
+     * # Method설명 : 특정 유저의 포인트 조회
+     * # MethodName : point
+     **/
+    public UserPoint point(long userId) {
+        return userPointTable.selectById(userId);
+    }
+
+
+    /**
+     * # Method설명 : 특정 유저의 포인트 충전/이용 내역 조회
+     * # MethodName : history
+     **/
+    public List<PointHistory> history(long userId) {
+        return pointHistoryTable.selectAllByUserId(userId);
+    }
+
+    /**
+     * # Method설명 : 특정 유저의 포인트 충전
+     * # MethodName : charge
+     **/
+    public UserPoint charge(long userId, long amount) {
+
+        if(amount <= 0){
+            throw new IllegalArgumentException("충전 금액은 1원 이상이어야 합니다.");
+        }
+
+        // 특정 유저의 포인트 조회
+        UserPoint userPoint = userPointTable.selectById(userId);
+
+        // 충전 포인트
+        long newPoint = userPoint.point() + amount;
+
+        if(newPoint > MAX_POINT){
+            throw new IllegalArgumentException("충전 시 최대 잔고("+ MAX_POINT +")를 초과할 수 없습니다.");
+        }
+
+        // 포인트 충전
+        UserPoint updatedUserPoint = userPointTable.insertOrUpdate(userId, newPoint);
+
+        // 포인트 충전 내역 저장
+        pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, System.currentTimeMillis());
+
+        return updatedUserPoint;
+    }
+
+    /**
+     * # Method설명 : 특정 유저의 포인트 사용
+     * # MethodName : use
+     **/
+    public UserPoint use(long userId, long amount) {
+
+        if(amount <= 0){
+            throw new IllegalArgumentException("사용 포인트는 1원 이상이어야 합니다.");
+        }
+
+        // 특정 유저의 포인트 조회
+        UserPoint userPoint = userPointTable.selectById(userId);
+        if (userPoint.point() < amount) {     // 보유 포인트가 사용할 포인트보다 적은 경우 사용 불가능
+            throw new IllegalArgumentException("잔고가 부족해 포인트를 사용할 수 없습니다.");
+        }
+
+        // 포인트 사용
+        UserPoint updatedUserPoint = userPointTable.insertOrUpdate(userId, userPoint.point() - amount);
+
+        // 포인트 사용 내역 저장
+        pointHistoryTable.insert(userId, amount, TransactionType.USE, System.currentTimeMillis());
+
+        return updatedUserPoint;
+    }
+
+}
